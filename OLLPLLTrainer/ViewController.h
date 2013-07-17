076be67ac17b4221b57d0aa6e417e2c028d78749
WChambers <chambers.weston@gmail.com>
2013-07-16 22:04:43 -0600

    Initial Commit

---
+++ OLLPLLTrainer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OLLPLLTrainer
+//
+//  Created by Weston Chambers on 7/16/13.
+//  Copyright (c) 2013 Weston Chambers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OLLPLLTrainerTests/OLLPLLTrainerTests.m
@@ -0,0 +1,36 @@
+//
+//  OLLPLLTrainerTests.m
+//  OLLPLLTrainerTests
+//
+//  Created by Weston Chambers on 7/16/13.
+//  Copyright (c) 2013 Weston Chambers. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OLLPLLTrainerTests : XCTestCase
+
+@end
+
+@implementation OLLPLLTrainerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
