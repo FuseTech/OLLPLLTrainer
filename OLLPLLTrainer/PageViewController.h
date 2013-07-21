a796db5f23462aa26760eb381b2ee406378372a7
WChambers <westong5@gmail.com>
2013-07-21 00:20:38 -0600

    Adding files for pageViewController

---
+++ OLLPLLTrainer/PageViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageViewController.h
+//  OLLPLLTrainer
+//
+//  Created by Weston Chambers on 7/21/13.
+//  Copyright (c) 2013 Weston Chambers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageViewController : UIPageViewController <UIPageViewControllerDelegate, UIPageViewControllerDataSource>
+
+@end
---
+++ OLLPLLTrainer/PickPiecesViewController.h
@@ -0,0 +1,16 @@
+//
+//  PickPiecesViewController.h
+//  OLLPLLTrainer
+//
+//  Created by Weston Chambers on 7/20/13.
+//  Copyright (c) 2013 Weston Chambers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickPiecesViewController : UIViewController
+
+
+
+
+@end
